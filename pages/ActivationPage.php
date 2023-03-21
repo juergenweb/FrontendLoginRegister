2f782923945e04b495b7b2a816ffbc6801d4fdcd
juergenweb <juergenweb@users.noreply.github.com>
2023-03-21 09:53:59 +0100

    Update ActivationPage.php

--- pages/ActivationPage.php
+++ pages/ActivationPage.php
@@ -39,18 +39,20 @@ class ActivationPage extends FrontendLoginRegisterPages
         $activation_code = $this->checkForQueryString('activationcode', false);
         $not_registered_code = $this->checkForQueryString('notregisteredcode', false);
 
-        if($not_registered_code != ''){
+        if ($not_registered_code != '') {
             // delete the user from the database
             $this->queryString = $not_registered_code;
             $this->verify = false;
-        } else if ($activation_code != ''){
-            // verify the user account
-            $this->queryString = $activation_code;
-            $this->verify = true;
         } else {
-            // no query string present, so redirect to the homepage
-            $url = $this->wire('pages')->get('/')->url;
-            $this->wire('session')->redirect($url);
+            if ($activation_code != '') {
+                // verify the user account
+                $this->queryString = $activation_code;
+                $this->verify = true;
+            } else {
+                // no query string present, so redirect to the homepage
+                $url = $this->wire('pages')->get('/')->url;
+                $this->wire('session')->redirect($url);
+            }
         }
 
         $this->alert = new Alert();
@@ -63,46 +65,47 @@ class ActivationPage extends FrontendLoginRegisterPages
      * @throws WireException
      * @throws WireException
      */
-    public function render(): string
+    public function render():string
     {
 
-            //check if user with this activation code exists in the database
-            if ($this->checkQueryStringUser('activation')) {
+        //check if user with this activation code exists in the database
+        if ($this->checkQueryStringUser('activation')) {
 
-                if($this->verify){
-                    // verify the account
-                    $this->user->setOutputFormatting(false);
-                    $this->user->fl_activation = ''; // delete the activation code
-                    $this->user->fl_activationdatetime = time(); // set the activation code time stamp
+            if ($this->verify) {
+                // verify the account
+                $this->user->setOutputFormatting(false);
+                $this->user->fl_activation = ''; // delete the activation code
+                $this->user->fl_activationdatetime = time(); // set the activation code time stamp
 
-                    // check if TfaEmail is installed and enable it for the user automatically
-                    if ($this->wire('modules')->isInstalled('TfaEmail')) {
-                        $tfa = new TfaEmail();
-                        if ($tfa->autoEnableSupported($this->user)) {
-                            $tfa->autoEnableUser($this->user);
-                        }
+                // check if TfaEmail is installed and enable it for the user automatically
+                if ($this->wire('modules')->isInstalled('TfaEmail')) {
+                    $tfa = new TfaEmail();
+                    if ($tfa->autoEnableSupported($this->user)) {
+                        $tfa->autoEnableUser($this->user);
                     }
-                    if ($this->user->save()) {
-                        // output success message
-                        $this->alert->setText(sprintf($this->_('Your account has been successfully activated. %s'),
-                            $this->___loginLink()->___render()));
-                        $this->alert->setCSSClass('alert_successClass');
-                    } else {
-                        // output warning message that a problem occurs during saving of user
-                        $this->savingUserProblemAlert();
-                    }
-                    $this->user->setOutputFormatting();
-                } else {
-                    // delete the account
-                    $this->user->delete();
-                    // set the alert
-                    $this->alert->setText(sprintf($this->_('Thank you for your feedback! The corresponding user account has been permanently deleted and you will not receive any further mails from %s.'), $this->wire('pages')->get('/')->httpUrl));
-                    $this->alert->setCSSClass('alert_successClass');
                 }
+                if ($this->user->save()) {
+                    // output success message
+                    $this->alert->setText(sprintf($this->_('Your account has been successfully activated. %s'),
+                        $this->___loginLink()->___render()));
+                    $this->alert->setCSSClass('alert_successClass');
+                } else {
+                    // output warning message that a problem occurs during saving of user
+                    $this->savingUserProblemAlert();
+                }
+                $this->user->setOutputFormatting();
             } else {
-                $this->alert->setText($this->_('Sorry, but something went wrong. This activation code has already been used, is not valid anymore or was not found in the database.'));
-                $this->alert->setCSSClass('alert_dangerClass');
+                // delete the account
+                $this->user->delete();
+                // set the alert
+                $this->alert->setText(sprintf($this->_('Thank you for your feedback! The corresponding user account has been permanently deleted and you will not receive any further mails from %s.'),
+                    $this->wire('pages')->get('/')->httpUrl));
+                $this->alert->setCSSClass('alert_successClass');
             }
+        } else {
+            $this->alert->setText($this->_('Sorry, but something went wrong. This activation code has already been used, is not valid anymore or was not found in the database.'));
+            $this->alert->setCSSClass('alert_dangerClass');
+        }
 
         return $this->alert->___render();
     }
