ab4b1361f9be3cffc109a6bb36859a10899845f6
juergenweb <juergenweb@users.noreply.github.com>
2023-03-27 15:34:39 +0200

    Update RegisterPage.php

--- pages/RegisterPage.php
+++ pages/RegisterPage.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 namespace FrontendLoginRegister;
-
+//checked 27.3
 /*
  * Class for creating a simple register form with at least username/email and password.
  * Can contain more fields if they were set in the backend config.
@@ -15,18 +15,15 @@ namespace FrontendLoginRegister;
  */
 
 use FrontendForms\Button as Button;
-use FrontendForms\Form as Form;
 use FrontendForms\Privacy as Privacy;
 use ProcessWire\TfaEmail;
-use ProcessWire\User as User;
+use ProcessWire\User;
 use ProcessWire\WireException;
 use ProcessWire\WireMail;
 use ProcessWire\WirePermissionException;
 
 class RegisterPage extends FrontendLoginRegisterPages
 {
-
-    use activation;
 
     /**
      * @throws WireException
@@ -70,19 +67,18 @@ class RegisterPage extends FrontendLoginRegisterPages
     /**
      * Method to check if activation code is still stored inside the db by a given email address
      * @param string $email
-     * @return string
+     * @return User|null
      * @throws WireException
      * @throws WirePermissionException
      */
-    protected function getActivationCode(string $email):string
+    protected function getUserByActivationCode(string $email):User|null
     {
         $user = $this->wire('users')->get('email=' . $email);
-        $code = '';
         // check if activation code exists
-        if ($user->id > 0) {
-            $code = $user->fl_activation;
+        if (($user->id > 0) && ($user->fl_activation)) {
+            return $user;
         }
-        return $code;
+        return null;
     }
 
     /**
@@ -116,11 +112,9 @@ class RegisterPage extends FrontendLoginRegisterPages
 
         if ($this->isValid()) {
 
-            //get and generate values first
+            //get the form values
             $email = $this->getValue('email');
             $pass = $this->getValue('pass');
-            $noEmailText = $this->_('If you do not see the email in a few minutes, check your “junk mail” folder or “spam” folder.');
-            $registerNewText = $this->_('If you still have not received an email, please fill out the registration form again to receive a new activation link.');
 
             $activationCode = $this->createQueryCode();// create the activation code
 
@@ -128,8 +122,9 @@ class RegisterPage extends FrontendLoginRegisterPages
             $newUser = new User();
             $newUser->of(false);
 
-            // set values for all field in the form
+            // set values for all fields inside the form
             $this->setFormFieldValues($newUser);
+
             // save the activation code
             $newUser->fl_activation = $activationCode;
 
@@ -140,15 +135,19 @@ class RegisterPage extends FrontendLoginRegisterPages
 
             if ($newUser->save()) {
 
+                // get the id of the user language as stored inside the db
+                $this->stored_user_lang = $this->getSavedUserLanguage($newUser);
+
+                // change user language to the stored user language placeholder in the stored user language
+                $this->user->setLanguage($this->stored_user_lang);
+
+                // add placeholders !!important!!
+                $this->createGeneralPlaceholders();
+
                 // if TFA email is enabled - add it to the user
-                if ($this->loginregisterConfig['input_tfa']) {
+                if (($this->loginregisterConfig['input_tfa']) && ($this->wire('modules')->isInstalled('TfaEmail'))) {
                     $tfa = new TfaEmail();
                     $tfa->autoEnableUser($newUser);
-                }
-
-                // set the language for the mail according to the user language on multi-language site
-                if ($this->wire('languages')) {
-                    $this->wire('languages')->setLanguage($newUser->language->name);
                 }
 
                 // create placeholders
@@ -165,12 +164,23 @@ class RegisterPage extends FrontendLoginRegisterPages
                 $this->setSenderName($m);
                 $m->subject($this->_('Action required to activate your account'));
                 $m->title($this->_('Please click the link to verify your registration'));
-                $m->bodyHTML($this->getLangValueOfConfigField('input_activationtext', $this->loginregisterConfig));
+                $m->bodyHTML($this->getLangValueOfConfigField('input_activationtext', $this->loginregisterConfig,
+                    $this->stored_user_lang->id));
                 $m->mailTemplate($this->loginregisterConfig['input_emailTemplate']);
-                if ($m->send()) {
+
+                $activation_mail_sent = $m->send();
+
+                // set back the language to the site language
+                $this->user->setLanguage($this->site_language_id);
+
+                if ($activation_mail_sent) {
+
+                    // add additional texts for the success alert
+                    $noEmailText = $this->_('If you do not see the email in a few minutes, check your “junk mail” folder or “spam” folder.');
+                    $registerNewText = $this->_('If you still have not received an email, please fill out the registration form again to receive a new activation link.');
+
                     $successMsg = $this->_('Your account has been created successfully. To activate your account, please follow the instructions inside the mail that has been sent to you.');
                     $this->getAlert()->setText($successMsg . '<br>' . $noEmailText . '<br>' . $registerNewText);
-
                 } else {
                     // output an error message that the mail could not be sent
                     $this->generateEmailSentErrorAlert();
@@ -182,19 +192,27 @@ class RegisterPage extends FrontendLoginRegisterPages
             $newUser->of(true);
         } else {
             // form is not valid, so check if email address exists but is not verified
-            // or in other words: check if user has been registered twice and has not activated his first registration
+            // or in other words: check if user has been registered twice and has not confirmed his first registration
             $email_field_name = $this->getAttribute('id') . '-email';
 
+            // check if there is an error inside the email field
             if (array_key_exists($email_field_name, $this->formErrors)) {
                 $email = $this->getValue($email_field_name);
+
                 // run only if an email address was entered
-                if (($email) && ($this->getActivationCode($email))) {
-                    // send reminder mail
-                    if ($this->sendReminderMail($this->wire('users')->get('email=' . $email))) {
-                        // overwrite alert message
-                        $this->getAlert()->setCSSClass('alert_warningClass')->setText($this->_('It seems that you have already registered before but have forgotten to activate your account. To activate your account, please follow the instructions inside the mail that have been sent to you.'));
-                        // do not display the form
-                        $this->showForm = false;
+                if ($email) {
+
+                    // get the user
+                    $user = $this->getUserByActivationCode($email);
+                    if (!is_null($user)) {
+
+                        // send reminder mail
+                        if ($this->sendReminderMail($user)) {
+                            // overwrite alert message
+                            $this->getAlert()->setCSSClass('alert_warningClass')->setText($this->_('It seems that you have already registered before but have forgotten to activate your account. To activate your account, please follow the instructions inside the mail that have been sent to you.'));
+                            // do not display the form
+                            $this->showForm = false;
+                        }
                     }
                 }
             }
