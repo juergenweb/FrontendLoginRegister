7ca08f32b97fff2a6c58cb9deedacb4097de2576
juergenweb <juergenweb@users.noreply.github.com>
2023-03-28 08:19:33 +0200

    Add files via upload

---
+++ mailtexts/de.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+namespace ProcessWire;
+
+/*
+ * German texts for the emails
+ * These texts will be saved to the database during the installation of the language file
+ *
+ * Created by Jürgen K.
+ * https://github.com/juergenweb 
+ * File name: de.php
+ * Created: 27.03.2023 
+ */
+
+// text for the email including the activation link
+$input_activationtext = <<<HEREA
+<p>Lieber Nutzer</p>
+<p>Sie erhalten diese E-Mail, weil Sie sich für ein Konto bei [[DOMAINVALUE]] am [[CURRENTDATETIMEVALUE]] registriert haben.<br />
+Um zu bestätigen, dass Sie die Person sind, die sich auf dieser Website registriert hat, klicken Sie bitte auf den nachfolgenden Link, um Ihr Konto zu aktivieren.</p>
+<p>[[VERIFICATIONLINK]]</p>
+<p>Nach der Überprüfung ist Ihr Konto aktiviert, und Sie können sich mit Ihren Anmeldedaten anmelden.</p>
+<p>Wenn Sie sich nicht für ein Konto registriert haben, ignorieren Sie bitte diese E-Mail oder klicken Sie auf den folgenden Link, um das Benutzerkonto zu löschen und keine weiteren E-Mails zu erhalten.</p>
+<p>[[NOTREGISTEREDLINK]]</p>
+HEREA;
+
+// text for the email including the tfa code
+$input_tfatext = <<<HEREA
+<p>Lieber Nutzer</p>
+<p>Bitte geben Sie den folgenden Code auf dem Bildschirm ein, um sich bei Ihrem Konto anzumelden.</p>
+<p>[[TFACODE]]</p>
+<p>Bitte beachten Sie: Dieser Code ist nur für [[EXPIRATIONTIME]] gültig.</p>
+HEREA;
+
+// text for the email to remind the user to activate his account
+$input_remindertext = <<<HEREA
+<p>Lieber Nutzer</p>
+<p>Sie erhalten diese E-Mail, weil Sie sich am [[REGISTRATIONDATE]] für einen Account bei [[DOMAINVALUE]] registriert haben.</p>
+<p>Leider haben Sie Ihren Account bis heute noch nicht aktiviert.<br />
+Ohne Aktivierung wird Ihr Account jedoch am [[DELETEDATE]] automatisch gelöscht.</p>
+<p>Um Ihr Konto zu aktivieren, klicken Sie bitte auf den folgenden Link, um Ihre Registrierung abzuschließen:</p>
+<p>[[VERIFICATIONLINK]]</p>
+<p>Wenn Sie sich nicht für ein Konto registriert haben, ignorieren Sie bitte diese E-Mail oder klicken Sie auf den folgenden Link, um das Benutzerkonto zu löschen und keine weiteren E-Mails mehr zu erhalten.</p>
+<p>[[NOTREGISTEREDLINK]]</p>
+HEREA;
+
+// text for the email containing a link to reset the login data
+$input_passwordforgottentext = <<<HEREA
+<p>Lieber Nutzer</p>
+<p>Sie erhalten diese E-Mail, weil jemand die [[LOGINDATA]] für Ihr Konto bei [[DOMAINVALUE]] angefordert hat.<br />
+Aus Sicherheitsgründen versenden wir die [[LOGINDATA]] nicht, daher müssen Sie neue [[RESETTEXT]] erstellen, indem Sie auf den folgenden Link klicken:</p>
+<p>[[RECOVERPASSWORDLINK]]</p>
+<p>Wenn Sie keine Rücksetzung der Anmeldedaten angefordert haben, ignorieren Sie bitte diese E-Mail.</p>
+HEREA;
+
+// text for the email containing a link to delete the account
+$input_deleteaccounttext = <<<HEREA
+<p>Lieber Nutzer</p>
+<p>Sie erhalten diese E-Mail, weil Sie einen Löschungslink für Ihr Konto bei [[DOMAINVALUE]] angefordert haben.<br />
+Bitte klicken Sie auf den Link, um Ihr Konto zu löschen. Bitte beachten Sie, dass dieser Link nur 5 Minuten lang gültig ist und nur funktioniert, wenn Sie eingeloggt sind.</p>
+<p>[[DELETEACCOUNTLINK]]</p>
+HEREA;
+
+// text for the email containing the information that the non-verified account is deleted
+$input_deletion_confirmation = <<<HEREA
+<p>Lieber Nutzer</p>
+<p>wir möchten Sie darüber informieren, dass Ihr Konto bei [[DOMAINVALUE]] nun am [[CURRENTDATETIMEVALUE]] gelöscht wurde.<br />
+Der Grund für die Löschung ist, dass Ihr Account seit Ihrer Registrierung am [[REGISTRATIONDATE]] nicht aktiviert wurde.<br />
+Um inaktiven Konten vorzubeugen, wird jedes nicht verifizierte Konto nach einer gewissen Zeit automatisch gelöscht.<br />
+Wenn Sie sich erneut registrieren möchten, besuchen Sie bitte [[REGISTERURL]] und registrieren Sie sich erneut.</p>
+<p>Wir wünschen Ihnen einen schönen Tag!</p>
+HEREA;
+
+// text for the mail to inform the user, that his account was blocked due to suspicious activities
+$input_unlock_account = <<<HEREA
+<p>Lieber Nutzer</p>
+<p>Sie erhalten diese E-Mail, weil wir verdächtige Aktivitäten auf Ihrem Benutzerkonto bei [[DOMAINVALUE]] festgestellt haben.<br />
+Jemand hat erfolglos versucht, sich in Ihr Konto einzuloggen, indem er Ihren [[LOGINTYPE]] in Kombination mit verschiedenen Passwörtern verwendet hat.<br />
+Aus Sicherheitsgründen haben wir Ihr Konto nun gesperrt.<br />
+Bitte klicken Sie auf den Link und folgen Sie den Anweisungen auf der weitergeleiteten Seite, um Ihr Konto zu entsperren.<br />
+Danach können Sie sich wieder normal einloggen.</p>
+<p>[[UNLOCKACCOUNTLINK]]</p>
+HEREA;
+
---
+++ mailtexts/en.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * English texts for the emails
+ * These texts will be saved to the database during the installation of the language file
+ *
+ * Created by Jürgen K.
+ * https://github.com/juergenweb 
+ * File name: en.php
+ * Created: 27.03.2023 
+ */
+
+
+// text for the email including the activation link
+$input_activationtext = <<<HEREA
+<p>Dear user</p>
+<p>You will receive this email, because you have registered for an account at [[DOMAINVALUE]] on [[CURRENTDATETIMEVALUE]].<br />
+To confirm that you are the person who registered on this site, please click the link below to activate your account.</p>
+<p>[[VERIFICATIONLINK]]</p>
+<p>After verification, your account is activated, and you can log in with your credentials.</p>
+<p>If you did not register for an account, please ignore this email or click the following link to delete the user account and to prevent getting further emails.</p>
+<p>[[NOTREGISTEREDLINK]]</p>
+HEREA;
+
+// text for the email including the tfa code
+$input_tfatext = <<<HEREA
+<p>Dear user</p>
+<p>Please enter the code below on the screen to login into your account.</p>
+<p>[[TFACODE]]</p>
+<p>Please note: This code is only valid for [[EXPIRATIONTIME]].</p>
+HEREA;
+
+// text for the email to remind the user to activate his account
+$input_remindertext = <<<HEREA
+<p>Dear user</p>
+<p>You are receiving this email because you registered for an account at [[DOMAINVALUE]] on [[REGISTRATIONDATE]].</p>
+<p>Unfortunately, you have not activated your account until today. However you have [[DAYSTODELETE]] left to activate your account before it will be deleted on [[DELETEDATE]].</p>
+<p>To activate your account, please click on the following link to complete your registration:</p>
+<p>[[VERIFICATIONLINK]]</p>
+<p>If you have not registered for an account, please ignore this email or click the following link to delete the account and stop receiving emails.</p>
+<p>[[NOTREGISTEREDLINK]]</p>
+HEREA;
+
+// text for the email containing a link to reset the login data
+$input_passwordforgottentext = <<<HEREA
+<p>Dear user</p>
+<p>You will receive this email, because someone has requested the [[LOGINDATA]] for your account at [[DOMAINVALUE]].<br />
+For security reasons, we do not send the [[LOGINDATA]], so you have to create the new [[RESETTEXT]] by clicking the following link:</p>
+<p>[[RECOVERPASSWORDLINK]]</p>
+<p>If you did not request a login data reset, please ignore this email.</p>
+HEREA;
+
+// text for the email containing a link to delete the account
+$input_deleteaccounttext = <<<HEREA
+<p>Dear user</p>
+<p>You will receive this email, because you have requested a deletion link for your account at [[DOMAINVALUE]].<br />
+Please click the link to delete your account, but please note that this link is only valid for 5 minutes, and it only works if you are logged in.</p>
+<p>[[DELETEACCOUNTLINK]]</p>
+HEREA;
+
+// text for the email containing the information that the non-verified account is deleted
+$input_deletion_confirmation = <<<HEREA
+<p>Dear user</p>
+<p>We want to inform you that your account at [[DOMAINVALUE]] has now been deleted on [[CURRENTDATETIMEVALUE]].<br />
+The reason for the deletion is that your account has not been activated since your registration on [[REGISTRATIONDATE]].<br />
+To prevent inactive accounts, every account not verified will be deleted after a certain time automatically.<br />
+If you want to register again, please visit [[REGISTERURL]] and register once more.</p>
+<p>We wish you a nice day!</p>
+HEREA;
+
+// text for the mail to inform the user, that his account was blocked due to suspicious activities
+$input_unlock_account = <<<HEREA
+<p>Dear user</p>
+<p>You will receive this email, because we have detected suspicious activity on your user account account at [[DOMAINVALUE]].<br />
+Someone has tried unsuccessfully to login into your account by using your [[LOGINTYPE]] in combination with various passwords.<br />
+For security reasons we have locked your account now.<br />
+Please click the link and follow the instructions on the redirected page to unlock your account.<br />
+After that you can log in normally again.</p>
+<p>[[UNLOCKACCOUNTLINK]]</p>
+HEREA;
+
