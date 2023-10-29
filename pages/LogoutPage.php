8b593ae5e9c5f39c50db012bd4372940319f01e8
juergenweb <juergenweb@users.noreply.github.com>
2023-10-29 19:43:22 +0100

    Update LogoutPage.php

--- pages/LogoutPage.php
+++ pages/LogoutPage.php
@@ -1,53 +1,52 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace FrontendLoginRegister;
+    namespace FrontendLoginRegister;
 
-// checked 27.3
-/*
- * Class for logging out a user if he is logged in
- * Returns an alert message that the user has been logged out successfully
- *
- * Created by Jürgen K.
- * https://github.com/juergenweb
- * File name: LogoutPage.php
- * Created: 06.07.2022
- */
-
-use FrontendForms\Alert as Alert;
-
-class LogoutPage extends FrontendLoginRegisterPages
-{
-
-    protected Alert $alert;
-
-    public function __construct()
-    {
-        // if user is guest -> redirect to homepage
-        $this->redirectToHomepage(true);
-
-        $this->wire('session')->logout();
-        $this->alert = new Alert();
-        $this->alert->setCSSClass('alert_successClass');
-        $this->alert->setText($this->_('You have been successfully logged out.'));
-
-    }
-
-    /**
-     * @return string
+    /*
+     * Class for logging out a user if he is logged in
+     * Returns an alert message that the user has been logged out successfully
+     *
+     * Created by Jürgen K.
+     * https://github.com/juergenweb
+     * File name: LogoutPage.php
+     * Created: 06.07.2022
      */
-    public function __toString():string
-    {
-        return $this->render();
-    }
 
-    /**
-     * Render the alert
-     * @return string
-     */
-    public function render():string
-    {
-        return $this->alert->___render();
-    }
+    use FrontendForms\Alert as Alert;
 
-}
+    class LogoutPage extends FrontendLoginRegisterPages
+    {
+
+        protected Alert $alert;
+
+        public function __construct()
+        {
+            // if user is guest -> redirect to homepage
+            $this->redirectToHomepage(true);
+
+            $this->wire('session')->logout();
+            $this->alert = new Alert();
+            $this->alert->setCSSClass('alert_successClass');
+            $this->alert->setText($this->_('You have been successfully logged out.'));
+
+        }
+
+        /**
+         * @return string
+         */
+        public function __toString(): string
+        {
+            return $this->render();
+        }
+
+        /**
+         * Render the alert
+         * @return string
+         */
+        public function render(): string
+        {
+            return $this->alert->___render();
+        }
+
+    }
