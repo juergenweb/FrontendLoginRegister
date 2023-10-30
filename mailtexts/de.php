c64f41c18365d23fe2520621c159d37a38d82ded
juergenweb <juergenweb@users.noreply.github.com>
2023-10-30 17:13:23 +0100

    Update de.php

--- mailtexts/de.php
+++ mailtexts/de.php
@@ -77,8 +77,8 @@ $input_unlock_account = <<<HEREA
 <p>Sie erhalten diese E-Mail, weil wir verdächtige Aktivitäten auf Ihrem Benutzerkonto bei [[DOMAINVALUE]] festgestellt haben.<br />
 Jemand hat erfolglos versucht, sich in Ihr Konto einzuloggen, indem er [[LOGINTYPE]] in Kombination mit verschiedenen Passwörtern verwendet hat.<br />
 Aus Sicherheitsgründen haben wir Ihr Konto nun gesperrt.<br />
-Bitte klicken Sie auf den Link und folgen Sie den Anweisungen auf der weitergeleiteten Seite, um Ihr Konto zu entsperren.<br />
-Danach können Sie sich wieder normal einloggen.</p>
+Bitte klicken Sie auf den Link und folgen Sie den entsprechenden Anweisungen, um Ihr Konto zu entsperren.<br />
+Danach können Sie sich wieder normal anmelden.</p>
 <p>[[UNLOCKACCOUNTLINK]]</p>
 HEREA;
 
