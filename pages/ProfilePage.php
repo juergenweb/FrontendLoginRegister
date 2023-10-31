8e16aaab8365e597d1558502a099e0836b59a97e
juergenweb <juergenweb@users.noreply.github.com>
2023-10-31 11:52:02 +0100

    Update ProfilePage.php

--- pages/ProfilePage.php
+++ pages/ProfilePage.php
@@ -50,6 +50,10 @@
             // create all form fields depending on the settings in the backend
             $this->createFormFields('input_profile', $this);
 
+            // show or hide the display of the toggle checkbox next to password fields
+            $this->getFormelementByName('pass')->showPasswordToggle((bool)(!$this->loginregisterConfig['input_hide_passwordtoggle']));
+            $this->getFormelementByName('pass-confirm')->showPasswordToggle((bool)(!$this->loginregisterConfig['input_hide_passwordtoggle']));
+
             // create and add the submission button to the form
             $button = new Button('submit');
             $button->setAttribute('value', $this->_('Update'));
